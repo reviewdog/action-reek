045be91200babfffbe01a26cbecb8ca1643b6753
Mikhail Grachev <work@mgrachev.com>
2020-05-22 19:17:27 +0300

    Update reviewdog
    Signed-off-by: Mikhail Grachev <work@mgrachev.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,4 +12,10 @@ export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 gem install -N reek $(version $INPUT_REEK_VERSION)
 
 reek --single-line . ${INPUT_REEK_FLAGS} \
-  | reviewdog -f=reek -name="${INPUT_TOOL_NAME}" -reporter="${INPUT_REPORTER}" -level="${INPUT_LEVEL}"
+  | reviewdog -f=reek \
+    -name="${INPUT_TOOL_NAME}" \
+    -reporter="${INPUT_REPORTER}" \
+    -filter-mode="${INPUT_FILTER_MODE}" \
+    -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
+    -level="${INPUT_LEVEL}" \
+    ${INPUT_REVIEWDOG_FLAGS}
